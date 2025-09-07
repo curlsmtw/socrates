7b7393ea590d92b289253cf7a3aa95757cf7cfae
Abbie Mathew <abbie.r.mathew@gmail.com>
2025-09-06 23:26:25 -0400

    feat: Add LineOverlapTextSplitter for chunking

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.document_loader import DocumentLoader
+from src.text_splitter import LineOverlapTextSplitter
 
 if __name__ == "__main__":
     loader = DocumentLoader("example_logs")
     documents = loader.load_logs()
-    print(documents)
+
+    splitter = LineOverlapTextSplitter(2, 1)
+    for doc in documents:
+        chunks = splitter.split(doc.page_content)
+        print(chunks)
